466c70b33cb9e91d56626c1474aa5f1074857ac4
user <user@JAYAPALKONDA>
2024-12-22 20:21:08 +0530

    some change

--- springBootProj6-usingWebDevelopment-RequestParamBinding1/src/main/java/com/nit/controllers/Controllers.java
+++ springBootProj6-usingWebDevelopment-RequestParamBinding1/src/main/java/com/nit/controllers/Controllers.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-//write logic
+//write logics
 
 @Controller
 public class Controllers {
